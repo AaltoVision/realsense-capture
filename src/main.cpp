8d1170c1128d087373bdc36cd6502ad3346e7232
Jerry Ylilammi <jerry.ylilammi@gmail.com>
2020-03-05 16:25:03 +0200

    Test different parameters from pose data

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,26 @@
 #include <iostream>
 #include <iomanip>
 
+
+void printPoseData(rs2_pose pose_data) {
+    /*  
+    rs2_vector 	translation
+    rs2_vector 	velocity
+    rs2_vector 	acceleration
+    rs2_quaternion 	rotation
+    rs2_vector 	angular_velocity
+    rs2_vector 	angular_acceleration
+    unsigned int 	tracker_confidence
+    unsigned int 	mapper_confidence
+    */
+    std::cout << "\r" << std::setprecision(3) << std::fixed
+        << "Pos[" << pose_data.translation.x << "," <<  pose_data.translation.y << "," << pose_data.translation.z << "]"
+        << ", Vel[" << pose_data.velocity.x << "," <<  pose_data.velocity.y << "," << pose_data.velocity.z << "]"
+        << ", Acc[" << pose_data.acceleration.x << "," <<  pose_data.acceleration.y << "," << pose_data.acceleration.z << "]"
+        ;
+}
+
+
 int main(int argc, char * argv[]) try
 {
     // Declare RealSense pipeline, encapsulating the actual device and sensors
@@ -26,8 +46,9 @@ int main(int argc, char * argv[]) try
         auto pose_data = f.as<rs2::pose_frame>().get_pose_data();
 
         // Print the x, y, z values of the translation, relative to initial position
-        std::cout << "\r" << "Device Position: " << std::setprecision(3) << std::fixed << pose_data.translation.x << " " <<
-            pose_data.translation.y << " " << pose_data.translation.z << " (meters)";
+        printPoseData(pose_data);
+        //std::cout << "\r" << "Device Position: " << std::setprecision(3) << std::fixed << pose_data.translation.x << " " <<
+        //    pose_data.translation.y << " " << pose_data.translation.z << " (meters)";
     }
 
     return EXIT_SUCCESS;
