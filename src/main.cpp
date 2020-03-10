e6d2080df36f667489e66d8a291289ebdad9d1d3
Jerry Ylilammi <jerry.ylilammi@gmail.com>
2020-03-10 13:48:14 +0200

    Better filenames

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,8 @@ std::string currentISO8601TimeUTC() {
   auto now = std::chrono::system_clock::now();
   auto itt = std::chrono::system_clock::to_time_t(now);
   std::ostringstream ss;
-  ss << std::put_time(gmtime(&itt), "%FT%TZ");
+  ss << std::put_time(gmtime(&itt), "%FT%H-%M-%SZ");
+
   return ss.str();
 }
 
